17af83926bab78c97de09eeb767ea36f6d7ea1ac
Chris Rice <chrisrice93@gmail.com>
2016-05-12 09:36:24 -0600

    First commit

---
+++ Assets/EnemyMouse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMouse : MonoBehaviour {
+
+    SpriteRenderer sprite_renderer;
+    public GameObject menu_controller_object;
+    MenuController menu_controller;
+
+    // Use this for initialization
+	void Start () {
+        sprite_renderer = gameObject.GetComponent<SpriteRenderer>();
+        menu_controller = menu_controller_object.GetComponent<MenuController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (!menu_controller.attack_button_pressed && !menu_controller.magic_button_pressed) {
+            sprite_renderer.color = new Color(1.0F, 1.0F, 1.0F);
+        }
+	}
+
+    void OnMouseEnter() {
+        if (menu_controller.attack_button_pressed) {
+            sprite_renderer.color = new Color(1.0F, 0.0F, 0.0F);
+        } else if (menu_controller.magic_button_pressed) {
+            sprite_renderer.color = new Color(0.0F, 0.0F, 1.0F);
+        }
+    }
+
+    void OnMouseExit() {
+        sprite_renderer.color = new Color(1.0F, 1.0F, 1.0F);
+    }
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    public bool attack_button_pressed = false;
+    public bool defend_button_pressed = false;
+    public bool magic_button_pressed = false;
+
+    public Texture2D default_cursor_texture;
+    public Texture2D attack_cursor_Texture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(default_cursor_texture, Vector2.zero, cursorMode);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("escape")) {
+            clearSelection();
+            Cursor.SetCursor(default_cursor_texture, Vector2.zero, cursorMode);
+        }
+
+        if (attack_button_pressed) {
+            Cursor.SetCursor(attack_cursor_Texture, hotSpot, cursorMode);
+        }
+	}
+
+    void attackButtonPressed() {
+        attack_button_pressed = true;
+    }
+
+    void defendButtonPressed() {
+        defend_button_pressed = true;
+    }
+
+    void magicButtonPressed() {
+        magic_button_pressed = true;
+    }
+
+    void clearSelection() {
+        attack_button_pressed = false;
+        defend_button_pressed = false;
+        magic_button_pressed = false;
+    }
+}
